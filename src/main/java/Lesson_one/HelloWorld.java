0f9adc3fda9d1ec5ee43f5280f151fa2b7746c0c
Maks Khomenko <khomenko.maks.qa@gmail.com>
2022-11-18 15:42:12 +0200

    add new three

--- src/main/java/Lesson_one/HelloWorld.java
+++ src/main/java/Lesson_one/HelloWorld.java
@@ -2,6 +2,12 @@ package Lesson_one;
 
 public class HelloWorld {
     public static void main(String[] args){
+
         System.out.println("Hello world! My name is Maksym");
+
+        System.out.println("QA Automation");
+        System.out.println("Yet one");
+        System.out.println("Yet two");
+
     }
 }
