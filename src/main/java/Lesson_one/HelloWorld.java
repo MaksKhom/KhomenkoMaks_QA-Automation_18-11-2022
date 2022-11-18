0cfe9e617964512bfa523ae43fbd5a9bead7aea9
Maks Khomenko <khomenko.maks.qa@gmail.com>
2022-11-18 16:02:52 +0200

    one

--- src/main/java/Lesson_one/HelloWorld.java
+++ src/main/java/Lesson_one/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
         System.out.println("Yet two");
         System.out.println("Somthing");
         System.out.println("Somthing two");
+        System.out.println("Somthing three");
     }
 }
