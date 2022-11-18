7ef41620320ca411da18ce734c807130229d3028
Maks Khomenko <khomenko.maks.qa@gmail.com>
2022-11-18 20:37:34 +0200

    Whaaa

--- src/main/java/Lesson_one/HelloWorld.java
+++ src/main/java/Lesson_one/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
         System.out.println("Hello world! My name is Maksym");
 
         System.out.println("QA Automation");
-
+        System.out.println("QA Automation";
     }
 }
