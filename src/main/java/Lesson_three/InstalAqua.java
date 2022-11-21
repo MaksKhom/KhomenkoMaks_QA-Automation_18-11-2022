d0da71c44eeccdac8eff7b5ba2cfedb6748d3c7e
Maks Khomenko <khomenko.maks.qa@gmail.com>
2022-11-21 21:11:22 +0200

    Instal Aqua!

--- src/main/java/Lesson_three/InstalAqua.java
+++ src/main/java/Lesson_three/InstalAqua.java
@@ -3,7 +3,7 @@ package Lesson_three;
 public class InstalAqua {
     public static void main(String[] args){
 
-        System.out.println("Aqua installed");
+        System.out.println("Aqua installed!");
     }
 
 
